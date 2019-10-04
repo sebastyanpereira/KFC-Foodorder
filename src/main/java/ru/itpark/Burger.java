b8730ab4d2dfe3ee28fa3798b9e813eb2c2f5478
dmitry.novikov <student@ya.ru>
2019-10-04 20:50:06 +0300

    Burger specification

--- src/main/java/ru/itpark/Burger.java
+++ src/main/java/ru/itpark/Burger.java
@@ -1,5 +1,14 @@
 package ru.itpark;
 
 public class Burger {
-
+    long id;
+    String name;
+    String imageUrl;
+    int cost;
+    int weight;
+    String description;
+    int energyValue;
+    double proteinQuantity;
+    double fatQuantity;
+    double carbohydrateQuantity;
 }
